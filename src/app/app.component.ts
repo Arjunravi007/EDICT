589f59ba371f0385af082ef5ceec03166f544363
ARAVINDHARAJ7 <aravindharaj931@gmail.com>
2022-03-12 14:27:14 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'EDICT';
+  logo:string ="EDICT\src\assets\images\logo.jpeg";
+}
